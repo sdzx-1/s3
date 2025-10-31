9e615fba351c0f4e33f44047efb53ce23f018d06
sdzx-1 <shangdizhixia1993@163.com>
2025-10-31 22:21:46 +0800

    Update build.zig

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const no_bin = b.option(bool, "no-bin", "skip emitting binary") orelse false;
 
     const sqlite = b.dependency("sqlite", .{ .target = target, .optimize = optimize });
 
@@ -34,32 +35,36 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    b.installArtifact(exe);
+    if (no_bin) {
+        b.getInstallStep().dependOn(&exe.step);
+    } else {
+        b.installArtifact(exe);
 
-    const run_step = b.step("run", "Run the app");
+        const run_step = b.step("run", "Run the app");
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
+        const run_cmd = b.addRunArtifact(exe);
+        run_step.dependOn(&run_cmd.step);
 
-    run_cmd.step.dependOn(b.getInstallStep());
+        run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
+    // const mod_tests = b.addTest(.{
+    //     .root_module = mod,
+    // });
 
-    const run_mod_tests = b.addRunArtifact(mod_tests);
+    // const run_mod_tests = b.addRunArtifact(mod_tests);
 
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
+    // const exe_tests = b.addTest(.{
+    //     .root_module = exe.root_module,
+    // });
 
-    const run_exe_tests = b.addRunArtifact(exe_tests);
+    // const run_exe_tests = b.addRunArtifact(exe_tests);
 
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    // const test_step = b.step("test", "Run tests");
+    // test_step.dependOn(&run_mod_tests.step);
+    // test_step.dependOn(&run_exe_tests.step);
 }
