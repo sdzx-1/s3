bbbe3a4c502822bc737d15deb16b735085a8568d
sdzx-1 <shangdizhixia1993@163.com>
2026-05-16 16:47:53 +0800

    Add dependency: troupe, zio

--- build.zig
+++ build.zig
@@ -4,6 +4,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    //troupe
+    const troupe = b.dependency("troupe", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    //zio
+    const zio = b.dependency("zio", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const tracy = b.option([]const u8, "tracy", "Enable Tracy integration. Supply path to Tracy source");
     const tracy_callstack = b.option(bool, "tracy-callstack", "Include callstack information with Tracy data. Does nothing if -Dtracy is not provided") orelse (tracy != null);
     const tracy_allocation = b.option(bool, "tracy-allocation", "Include allocation information with Tracy data. Does nothing if -Dtracy is not provided") orelse (tracy != null);
@@ -15,19 +27,16 @@ pub fn build(b: *std.Build) void {
     options.addOption(bool, "enable_tracy_allocation", tracy_allocation);
     options.addOption(u32, "tracy_callstack_depth", tracy_callstack_depth);
 
-    const mod = b.addModule("s3", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
-
     const exe = b.addExecutable(.{
         .name = "s3",
+        .use_llvm = true, //arch sframe bug: https://codeberg.org/ziglang/zig/issues/30959
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                .{ .name = "s3", .module = mod },
+                .{ .name = "troupe", .module = troupe.module("root") },
+                .{ .name = "zio", .module = zio.module("zio") },
             },
             .strip = if (tracy != null) false else if (optimize == .Debug) false else true,
         }),
@@ -67,19 +76,10 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
-
     const run_exe_tests = b.addRunArtifact(exe_tests);
-
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
