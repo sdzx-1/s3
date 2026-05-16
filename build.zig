e8cb61266c89ac203882f0e302a1b6d68b2ed014
sdzx-1 <shangdizhixia1993@163.com>
2026-05-16 11:39:25 +0800

    Impl trace, allow tracking of function memory usage

--- build.zig
+++ build.zig
@@ -4,6 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const tracy = b.option([]const u8, "tracy", "Enable Tracy integration. Supply path to Tracy source");
+    const tracy_callstack = b.option(bool, "tracy-callstack", "Include callstack information with Tracy data. Does nothing if -Dtracy is not provided") orelse (tracy != null);
+    const tracy_allocation = b.option(bool, "tracy-allocation", "Include allocation information with Tracy data. Does nothing if -Dtracy is not provided") orelse (tracy != null);
+    const tracy_callstack_depth: u32 = b.option(u32, "tracy-callstack-depth", "Declare callstack depth for Tracy data. Does nothing if -Dtracy_callstack is not provided") orelse 10;
+
+    const options = b.addOptions();
+    options.addOption(bool, "enable_tracy", tracy != null);
+    options.addOption(bool, "enable_tracy_callstack", tracy_callstack);
+    options.addOption(bool, "enable_tracy_allocation", tracy_allocation);
+    options.addOption(u32, "tracy_callstack_depth", tracy_callstack_depth);
+
     const mod = b.addModule("s3", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -18,8 +29,30 @@ pub fn build(b: *std.Build) void {
             .imports = &.{
                 .{ .name = "s3", .module = mod },
             },
+            .strip = if (tracy != null) false else if (optimize == .Debug) false else true,
         }),
     });
+
+    exe.root_module.addOptions("build_options", options);
+
+    if (tracy) |tracy_path| {
+        const client_cpp = b.pathJoin(
+            &[_][]const u8{ tracy_path, "public", "TracyClient.cpp" },
+        );
+        const tracy_c_flags: []const []const u8 = &.{
+            "-DTRACY_ENABLE=1",
+            "-fno-sanitize=undefined",
+            //TODO: tracy fiber
+        };
+
+        exe.root_module.addIncludePath(.{ .cwd_relative = tracy_path });
+        exe.root_module.addCSourceFile(.{
+            .file = .{ .cwd_relative = client_cpp },
+            .flags = tracy_c_flags,
+        });
+        exe.root_module.link_libc = true;
+        exe.root_module.link_libcpp = true;
+    }
 
     b.installArtifact(exe);
 
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,23 @@
 const std = @import("std");
 const Io = std.Io;
+const tracy = @import("tracy.zig");
+const trace = tracy.trace;
 
 const s3 = @import("s3");
 
 pub fn main(init: std.process.Init) !void {
-    _ = init;
+    const io = init.io;
+    tracy.frameMark();
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    for (0..100) |_| {
+        try io.sleep(.fromSeconds(1), .awake);
+        foo();
+    }
+}
+
+fn foo() void {
+    const tracy_fun = trace(@src());
+    defer tracy_fun.end();
+
+    std.debug.print("some thing", .{});
 }
